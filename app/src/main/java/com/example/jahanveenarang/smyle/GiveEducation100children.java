3c4d7be746dc66a89d0d78680ab9bca69f02e5fa
tripti <tripti97.sharma@gmail.com>
2019-01-11 16:36:56 +0530

    Tripti's commit

--- app/src/main/java/com/example/jahanveenarang/smyle/GiveEducation100children.java
+++ app/src/main/java/com/example/jahanveenarang/smyle/GiveEducation100children.java
@@ -4,18 +4,20 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class GiveEducation100children extends AppCompatActivity {
 
 
     Toolbar toolbar;
-    Button bt_ed_donate, bt_ed_call;
+    Button bt_ed_donate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,36 +27,52 @@ public class GiveEducation100children extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar_education);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Sponser Girl Educattion");
+        getSupportActionBar().setTitle("Sponsor Girl Education");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        bt_ed_donate = findViewById(R.id.bt_educate_donate);
-        bt_ed_call = findViewById(R.id.bt_educate_call);
-
-        bt_ed_call.setOnClickListener(new View.OnClickListener() {
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                String posted_by = "9910191122";
-
-                String uri = "tel:" + posted_by.trim();
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse(uri));
-                if (ActivityCompat.checkSelfPermission(GiveEducation100children.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                startActivity(intent);
-
+            public void onClick(View v) {
+                finish();
             }
         });
 
+
+
+        bt_ed_donate = findViewById(R.id.bt_educate_donate);
+
+
+    }
+    public void dial(View view){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            call("9910191122");
+        }else {
+            String[] permissions = {Manifest.permission.CALL_PHONE};
+            ActivityCompat.requestPermissions(this,permissions,1);
+        }
+
+    }
+
+    public void call(String phone){
+        Intent intent=new Intent();
+        intent.setAction(Intent.ACTION_CALL);
+        Uri uri=Uri.parse("tel:"+phone);
+        intent.setData(uri);
+        startActivity(intent);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            int callGrantResult=grantResults[0];
+            if(callGrantResult==PackageManager.PERMISSION_GRANTED){
+                call("9910191122");
+
+            }else{
+                Toast.makeText(this,"Grant permission",Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
